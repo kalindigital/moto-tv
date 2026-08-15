3667ad5660c9b4f94719d06336e704d2e7010e63
Kalin Digital <102815325+kalindigital@users.noreply.github.com>
2026-08-15 18:00:23 -0300

    Versão 0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.mototv"
         minSdk = 21
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.7.0"
     }
 
     signingConfigs {
